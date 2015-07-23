e02cdc140d425f1696a5810191af91c0d1453920
perterest <qingfengpei@gmail.com>
2015-07-23 17:30:45 +0800

    提交基本代码结构

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lightApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lightApp
+ */
+angular.module('lightApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lightApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lightApp
+ */
+angular.module('lightApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
